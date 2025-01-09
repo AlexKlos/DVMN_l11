c697a88a707267293275a2e538ec65da9a3b9c98
AlexKlos <klos@list.ru>
2025-01-10 01:10:52 +0700

    Removed templates if __name__

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -3,14 +3,9 @@ import argparse
 from main import download_files, get_spacex_launch_image
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Download photos from the SpaceX launch')
-    parser.add_argument('id', type=str, nargs='?', help='SpaceX launch ID')
-    args = parser.parse_args()
-    id = args.id if args.id else 'latest'
+parser = argparse.ArgumentParser(description='Download photos from the SpaceX launch')
+parser.add_argument('id', type=str, nargs='?', help='SpaceX launch ID')
+args = parser.parse_args()
+id = args.id if args.id else 'latest'
 
-    download_files(get_spacex_launch_image(id), 'images', 'spacex')
-
-
-if __name__ == '__main__':
-    main()
+download_files(get_spacex_launch_image(id), 'images', 'spacex')
