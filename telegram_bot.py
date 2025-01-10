575a73faa373c10403945819b120c620afe3e1a6
AlexKlos <klos@list.ru>
2025-01-10 14:32:27 +0700

    Finished telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,17 +1,86 @@
 import os
+import random
+import time
 
-import telegram
 from dotenv import load_dotenv
+import telegram
+
+
+def get_file_list(folder: str = 'images') -> list:
+    '''Gets a list of file paths for all files in the specified folder and its subfolders.
+
+    Args:
+        folder (str, optional): Path to the folder to scan. Defaults to 'images'.
+
+    Returns:
+        list: A list of full file paths for all files in the folder and its subfolders. 
+              Returns an empty list if an error occurs.
+
+    Raises:
+        FileNotFoundError: If the specified folder does not exist.
+        PermissionError: If there is insufficient permission to access the folder or its files.
+    '''
+    try:
+        pathes = []
+        for curent_folder, folders, files in os.walk(folder):
+            for file in files:
+                path = os.path.join(curent_folder, file)
+                pathes.append(path)
+        return pathes
+    except Exception as e:
+        print(f'Error: {e}')
+        return []
+
+
+def post_images_from_folder(bot: telegram.Bot, 
+                            chat_id: str, 
+                            folder: str, 
+                            pause: int, 
+                            shuffle: bool=False) -> bool:
+    '''Posts all images from folder to a Telegram chat.
+
+    Args:
+        bot (telegram.Bot): Instance of the Telegram bot.
+        chat_id (str): ID of the chat where images will be posted.
+        folder (str): Path to the folder containing images.
+        pause (int): Pause between posts in seconds.
+        shuffle (bool, optional): Determines the order of image posting. 
+            If False, images are posted in order; 
+            if True, the order is randomized. Defaults to False.
+
+    Returns:
+        bool: True if all images are successfully read and posted, otherwise False.
+
+    Raises:
+        FileNotFoundError: If the specified folder does not exist.
+        telegram.error.TelegramError: If there is an error sending a photo via Telegram.
+        OSError: If there is an issue reading a file from the folder.
+    '''
+    files = get_file_list(folder)
+    if shuffle:
+        random.shuffle(files)
+    for file in files:
+        try:
+            with open(file, 'rb') as image:
+                bot.send_photo(chat_id, photo=image)
+            time.sleep(pause)
+        except Exception as e:
+            print(f'Error: {e}')
+            return False
+    return True
 
 
 def main():
     load_dotenv()
     bot = telegram.Bot(token=os.environ['TELEGRAM_API_TOKEN'])
-    chat_id = os.environ['TELEGRAM_CHAT_ID']
+    CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
+    PAUSE = int(os.environ.get('PAUSE_BETWEEN_POSTS', 14400))
+    folder = 'images'
 
-    image_path = 'images/nasa_apod_0.jpg'
-    with open(image_path, 'rb') as image:
-        bot.send_photo(chat_id=chat_id, photo=image)
+    shuffle = False
+    while True:
+        print(post_images_from_folder(bot, CHAT_ID, folder, PAUSE, shuffle))
+        shuffle = True
 
 
 if __name__ == '__main__':
