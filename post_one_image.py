9915291122970bdeda120308458329fbe6c3ae22
AlexKlos <klos@list.ru>
2025-01-17 13:27:14 +0700

    Исправлено - Сократите размер блоков try...except

--- post_one_image.py
+++ post_one_image.py
@@ -18,29 +18,29 @@ def main():
         FileNotFoundError: If the specified image file or folder does not exist.
         telegram.error.TelegramError: If there is an issue sending the photo to Telegram.
     """
-    try:
-        load_dotenv()
-        bot = telegram.Bot(token=os.environ['TELEGRAM_API_TOKEN'])
-        chat_id = os.environ['TELEGRAM_CHAT_ID']
-    
-        parser = argparse.ArgumentParser(description='Post one image to telegram')
-        parser.add_argument('file', type=str, nargs='?', help='filename')
-        args = parser.parse_args()
-    
-        folder = 'images'
-        if args.file:
-            file = os.path.join(folder, args.file)
-        else:
-            file = random.choice(get_file_list(folder))
-    
-        with open(file, 'rb') as image:
-            bot.send_photo(chat_id, photo=image)
-            
-    except FileNotFoundError as e:
-        print(f"Error: {e}")
-    except telegram.error.TelegramError as e:
-        print(f"Telegram error: {e}")
+    load_dotenv()
+    bot = telegram.Bot(token=os.environ['TELEGRAM_API_TOKEN'])
+    chat_id = os.environ['TELEGRAM_CHAT_ID']
 
+    parser = argparse.ArgumentParser(description='Post one image to telegram')
+    parser.add_argument('file', type=str, nargs='?', help='filename')
+    args = parser.parse_args()
+
+    folder = 'images'
+    if args.file:
+        file = os.path.join(folder, args.file)
+    else:
+        try:
+            file = random.choice(get_file_list(folder))
+        except FileNotFoundError as e:
+            print(f"Error: The folder '{folder}' was not found. {e}")
+
+    with open(file, 'rb') as image:
+        try:
+            bot.send_photo(chat_id, photo=image)
+        except telegram.error.TelegramError as e:
+            print(f"Telegram error while sending file: {e}")
+            
 
 if __name__ == '__main__':
     main()
